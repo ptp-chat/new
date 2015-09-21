ae718ef89b0c384e001e44f5fc1c8d6fae88b175
JamenMarz <jamen.marz@gmail.com>
2015-09-21 00:53:28 -0600

    Add sticky nav.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,22 @@
 (function($){
   'use strict';
+  $(function(){
+
+  var base = $(window),
+      head = $('#itop'),
+      nav = $('nav');
+
+  base.on('scroll', function(){
+    if (base.scrollTop() > head.outerHeight()) {
+      if (!nav.hasClass('sticky')) {
+        nav.addClass('sticky');
+      }
+    } else {
+      if (nav.hasClass('sticky')) {
+        nav.removeClass('sticky');
+      }
+    }
+  });
+
+  })
 }(jQuery));
